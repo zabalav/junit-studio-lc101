6a517758f439ccbf31c9ee1d824858f294a13c10
zabalav <vincent_zabala@yahoo.com>
2021-05-06 20:04:41 -0500

    studio complete

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,7 +1,7 @@
 package test;
 
 import org.junit.Test;
-
+import main.BalancedBrackets;
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -12,5 +12,69 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+//"[LaunchCode]", "Launch[Code]", "[]LaunchCode", "",
+    @Test
+    public void bracketsEnclosingWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void bracketsEnclosingPartialWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void bracketsEnclosedBeforeWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    //"[LaunchCode", "Launch]Code[", "[", "]["
+    @Test
+    public void onlyLeftBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void oneLeftAndRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void oneLeftBracketBeforeWord() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void oneLeftBracketBeforeRight() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void oneLeftLeftRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[launch[]"));
+    }
 
 }
+//""
+//"[]"
+//"[LaunchCode]"
+//"Launch[Code]"
+//"[]LaunchCode"
+//"[]LaunchCode[]"
+//"[[LaunchCode]]"
+//"]["
+//"[LaunchCode"
+//"Launch]Code["
+//"["
+//"[launch[]"
+//"[launch[]"
+//"]launch[]"
